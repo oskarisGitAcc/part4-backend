bca91a75e29877e6495b441aadfe2691c859dacd
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-02-06 12:31:14 +0200

    add middleware

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
 const mongoUrl = config.MONGODB_URI
 
 mongoose.set('strictQuery',false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
 mongoose.connect(mongoUrl).then(() => {
   logger.info('connected to MongoDB')
 }).catch((error) => {
@@ -22,27 +26,12 @@ morgan.token('postData', (req) => {
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 app.use(express.json())
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
-app.use(unknownEndpoint)
-app.use(errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
