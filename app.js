54717065658203c7c1417204f1b622c9843ffb03
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-02-06 12:34:54 +0200

    4.2 Blog list, step2
    Complete task 4.2 by adding app.js and importing it in index.js.

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(mongoUrl).then(() => {
+  logger.info('connected to MongoDB')
+}).catch((error) => {
+  logger.error('error connecting to mongoDB: ', error.message)
+})
+
+morgan.token('postData', (req) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
