699c27b700a6fd86a5e6d312dfd0a8dbc8a8c2dc
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-02-06 17:38:53 +0200

    4.9: Blog list tests, step2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,12 +8,10 @@ const Blog = require('../models/blog')
 beforeEach(async () => {
   try {
     await Blog.deleteMany({})
-    const insertedBlogs = await Blog.insertMany(helper.initialBlogs)
-    console.log('Inserted blogs:', insertedBlogs)
+    await Blog.insertMany(helper.initialBlogs)
   } catch (error) {
     console.error('Error inserting blogs:', error)
   }
-
 })
 
 test('blogs are returned as json', async () => {
@@ -26,6 +24,12 @@ test('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('blog post has id property', async () => {
+  const blogs = await helper.blogsInDb()
+  const firstBlog = blogs[0]
+  expect(firstBlog.id).toBeDefined()
 })
 
 afterAll(async () => {
