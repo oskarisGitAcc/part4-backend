3a7c126c99a28362d6edaf6ee6f45ee7cb55ddb2
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-02-06 13:46:40 +0200

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,24 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const mostLikedBlog = blogs.reduce((max, blog) => {
+    return blog.likes > max.likes ? blog : max
+  }, blogs[0])
+
+  return {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
